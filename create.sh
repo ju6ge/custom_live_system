374c8bdd1bb5ff7b0b83398e23f93b147785d053
ju6ge <judge@felixrichter.tech>
2023-12-06 19:11:58 +0100

    working iso for boot

--- create.sh
+++ create.sh
@@ -1,30 +1,32 @@
 #!/bin/zsh
 
+iso_label="RESCUEARCH"
+
 build_dir=/tmp/rescue_system
 
 mkdir -p $build_dir
 
 rescue_img=$build_dir/rescue_arch_zfs.img
 
-dd if=/dev/zero of=$rescue_img bs=1M count=3000 2>/dev/null
-echo "Created $rescue_img"
-
-# partition image
-sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk $rescue_img
-  g # clear the in memory partition table
-  n # new partition
-  1 # partition number 1
-    # default - start at beginning of disk
-  +100M # 100 MB boot parttion
-  t # change partion type
-  1 # set to efi system partition
-  n # new partition
-  2 # partion number 2
-    # default, start immediately after preceding partition
-    # default, extend partition to end of disk
-  p # print the in-memory partition table
-  w # write the partition table
-EOF
+#dd if=/dev/zero of=$rescue_img bs=1M count=3000 2>/dev/null
+#echo "Created $rescue_img"
+#
+## partition image
+#sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk $rescue_img
+#  g # clear the in memory partition table
+#  n # new partition
+#  1 # partition number 1
+#    # default - start at beginning of disk
+#  +100M # 100 MB boot parttion
+#  t # change partion type
+#  1 # set to efi system partition
+#  n # new partition
+#  2 # partion number 2
+#    # default, start immediately after preceding partition
+#    # default, extend partition to end of disk
+#  p # print the in-memory partition table
+#  w # write the partition table
+#EOF
 
 efi_offset=$(sfdisk -J $rescue_img | jq '.partitiontable.partitions.[0].start')
 efi_size=$(sfdisk -J $rescue_img | jq '.partitiontable.partitions.[0].size')
@@ -37,8 +39,8 @@ root_loop=$(losetup -o $(($root_offset*512)) --sizelimit $((($root_size-1)*512))
 root_mount=$build_dir/root
 efi_mount=$root_mount/boot/efi
 
-mkfs.fat -F 32 $efi_loop
-mkfs.ext4 $root_loop
+#mkfs.fat -F 32 $efi_loop
+#mkfs.ext4 $root_loop
 
 mkdir -p $root_mount
 mount $root_loop $root_mount
@@ -46,9 +48,8 @@ mount $root_loop $root_mount
 mkdir -p $efi_mount
 mount $efi_loop $efi_mount
 
-pacstrap $root_mount base base-devel dracut linux-lts linux-lts-headers zfs-linux-lts
+#pacstrap $root_mount base base-devel dracut linux-lts linux-lts-headers dkms zfs-dkms squashfs-tools
 
-genfstab -U $root_mount > $root_mount/etc/fstab
 echo "rescuesystem" > $root_mount/etc/hostname
 echo "KEYMAP=de" > $root_mount/etc/vconsole.conf
 echo "KEYMAP_TOGGLE=neo" >> $root_mount/etc/vconsole.conf
@@ -69,7 +70,12 @@ mkdir -p $efi_mount/EFI/BOOT
 kernel_version=$(ls $root_mount/usr/lib/modules | jq -R | jq -s -c | jq -r '.[0]')
 
 # create efi kernel for iso booting
-echo 'kernel_cmdline="iso-scan/filename=rootfs.sfs root=/run/initramfs/isoscandev/rootfs.sfs rootfstype=sqashfs"' > $root_mount/etc/dracut.conf.d/liveiso.conf
+cat <<EOF > $root_mount/etc/dracut.conf.d/liveiso.conf
+filesystem=" squashfs "
+add_dracutmodules=" systemd-udevd dmsquash-live "
+kernel_cmdline=" root=live:CDLABEL=$iso_label rd.live.squashimg=rootfs.sfs rd.live.overlay.overlayfs ro "
+EOF
+#kernel_cmdline=" rescue "
 arch-chroot $root_mount dracut --force --uefi --uefi-stub /usr/lib/systemd/boot/efi/linuxx64.efi.stub /boot/efi/EFI/BOOT/BOOTX64.EFI --kver $kernel_version
 sync
 
@@ -84,11 +90,16 @@ rm $root_mount/etc/dracut.conf.d/liveiso.conf
 echo 'kernel_cmdline="root=UUID='$root_fs_uuid'"' > $root_mount/etc/dracut.conf.d/cmdline.conf
 arch-chroot $root_mount dracut --force --uefi --uefi-stub /usr/lib/systemd/boot/efi/linuxx64.efi.stub /boot/efi/EFI/BOOT/BOOTX64.EFI --kver $kernel_version
 
-mksquashfs $root_mount $iso_dir/rootfs.sfs -comp xz
+rm $root_mount/etc/fstab
+
+mkdir -p $iso_dir/LiveOS
+#rm $iso_dir/LiveOS/rootfs.sfs
+#mksquashfs $root_mount $iso_dir/LiveOS/rootfs.sfs -comp xz
 mkdir -p $iso_dir/isolinux
 cp /usr/lib/syslinux/bios/isolinux.bin $iso_dir/isolinux
 
 xorriso -as mkisofs \
+  -V $iso_label \
   -o $build_dir/output.iso \
   -isohybrid-mbr /usr/lib/syslinux/bios/isohdpfx.bin \
   -b isolinux/isolinux.bin \
@@ -99,8 +110,9 @@ xorriso -as mkisofs \
    -isohybrid-gpt-basdat \
   $iso_dir
 
+genfstab -U $root_mount > $root_mount/etc/fstab
+
 umount $efi_mount
 umount $root_mount
 losetup -d $efi_loop
 losetup -d $root_loop
-
